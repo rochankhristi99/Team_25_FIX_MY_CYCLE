ebd6e350378e7b4d089a0521f6431d4d8b56795c
Rabaya Keya <rabayakeya@gmail.com>
2024-02-08 23:20:02 +0200

    Login Page (In progress)

--- db.php
+++ db.php
@@ -1,14 +1,8 @@
 <?php
-
 $servername = "db";
 $username = "fix_my_cycle";
 $password = "abc@123";
 $dbname = "fix_my_cycle_db";
-
-// $servername = "localhost";
-// $username = "rochan23000";
-// $password = "omt1zrrS";
-// $dbname = "wp_rochan23000";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
